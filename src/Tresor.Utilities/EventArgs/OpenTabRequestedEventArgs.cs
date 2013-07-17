88162e20639bf089d94cc06d30450d57bdbb6687
Lucas Schad <schad.lucas@gmail.com>
2013-07-17 23:31:24 +0200

    Basisklasse für ViewModels um Ereignis zum Tab Öffnen erweitert.

---
+++ src/Tresor.Utilities/EventArgs/OpenTabRequestedEventArgs.cs
@@ -0,0 +1,27 @@
+﻿namespace Tresor.Utilities.EventArgs
+{
+    using System;
+
+    using Tresor.Contracts.Utilities;
+
+    /// <summary>EventArgs für ein Argument, dass einen Tab öffnen möchte.</summary>
+    public class OpenTabRequestedEventArgs<T> : EventArgs
+    {
+        /// <summary>Mitglied der Eigenschaft <see cref="Content"/>.</summary>
+        private T content;
+
+        /// <summary>Holt oder setzt das Objekt, welches geöffnet werden soll.</summary>
+        public T Content
+        {
+            get
+            {
+                return content;
+            }
+
+            set
+            {
+                content = value;
+            }
+        }
+    }
+}
--- src/Tresor.ViewModel/ViewModel.cs
+++ src/Tresor.ViewModel/ViewModel.cs
@@ -1,8 +1,41 @@
 ﻿namespace Tresor.ViewModel
 {
-    using Tresor.Utilities;
+    using System;
 
+    using Tresor.Contracts.Utilities;
+    using Tresor.Utilities;
+    using Tresor.Utilities.EventArgs;
+
+    /// <summary>Basisklasse für ViewModels.</summary>
     public abstract class ViewModel : NotifyPropertyChanged
     {
+        #region Öffentliche Ereignisse
+
+        /// <summary>Ereignis das ausgelöst wird, wenn ein neuer Tab geöffnet werden soll.</summary>
+        public virtual event EventHandler<OpenTabRequestedEventArgs<object>> OpenTabRequested;
+
+        #endregion
+
+        #region Methoden
+
+        /// <summary>Informiert Abonnenten von <see cref="OpenTabRequested"/> darüber, dass das Ereignis ausgelöst wurde.</summary>
+        /// <param name="arguments">Die Argumente, welche zu dem Ereignis gehören.</param>
+        protected virtual void OnOpenTabEvent(OpenTabRequestedEventArgs<object> arguments)
+        {
+            var handler = OpenTabRequested;
+
+            if (handler != null)
+            {
+                handler(this, arguments);
+            }
+        }
+
+        /// <summary>Öffnet einen neuen Tab.</summary>
+        /// <param name="password">Das Passwort, welches dargestellt werden soll.</param>
+        protected virtual void OpenTab(IPassword password)
+        {
+        }
+
+        #endregion
     }
 }
