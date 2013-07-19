e85a7acddf15f137a5471f3e777cbf89a5a4fc51
Lucas Schad <schad.lucas@gmail.com>
2013-07-19 17:37:26 +0200

    Tabs können nun mit Mouse3 geschloßen werden.

--- src/Tresor/Bootstrapper.cs
+++ src/Tresor/Bootstrapper.cs
@@ -1,18 +1,26 @@
 ﻿namespace Tresor
 {
     using System.Windows.Controls;
+    using System.Windows.Input;
 
     using Microsoft.Practices.Unity;
 
     using global::Tresor.Contracts.Model;
+
+    using global::Tresor.Contracts.Utilities;
+
     using global::Tresor.Contracts.ViewModel;
 
     using global::Tresor.Model;
+
     using global::Tresor.Utilities.EventArgs;
+
     using global::Tresor.View;
 
     using global::Tresor.View.Application;
+
     using global::Tresor.View.UserControls;
+
     using global::Tresor.ViewModel;
 
     /// <summary>Statischer Bootstrapper der die Anwendung verkabelt.</summary>
@@ -22,6 +30,9 @@
 
         /// <summary>Der IoC Container.</summary>
         private readonly IUnityContainer container = new UnityContainer();
+
+        /// <summary>Das Applikationsfenster.</summary>
+        private MainWindow mainWindow;
 
         #endregion
 
@@ -38,6 +49,28 @@
 
         #region Methoden
 
+        /// <summary>Holt ein neues TabItem, welches ein Passwort darstellt.</summary>
+        /// <param name="password">Das darzustellende Passwort.</param>
+        /// <returns>Das neu erzeuget <see cref="TabItem"/>.</returns>
+        private TabItem GetNewPasswordTab(IPassword password)
+        {
+            var tabItem = GetNewTabItem(password.Account, "Passwort bearbeiten", container.Resolve<PasswordView>());
+            return tabItem;
+        }
+
+        /// <summary>Holt ein neues TabItem.</summary>
+        /// <param name="header">Der Header des TabItems.</param>
+        /// <param name="toolTip">Das Tooltip des TabItems.</param>
+        /// <param name="content">Der Inhalt des TabItems.</param>
+        /// <returns>Das neu erzeugte <see cref="TabItem"/>.</returns>
+        private TabItem GetNewTabItem(string header, string toolTip, object content)
+        {
+            var tabItem = new TabItem { Header = header, ToolTip = toolTip, Content = content };
+            tabItem.MouseUp += TabItemClicked;
+
+            return tabItem;
+        }
+
         /// <summary>Erstellt die <see cref="Tresor.View.PanelView"/> und gibt diese zurück.</summary>
         /// <returns>Die <see cref="Tresor.View.PanelView"/>.</returns>
         private PanelView GetPanelView()
@@ -47,6 +80,14 @@
             panelViewModel.OpenTabRequested += OpenTabRequested;
             panelView.DataContext = panelViewModel;
             return panelView;
+        }
+
+        /// <summary>Lädt das Hauptfenster.</summary>
+        private void LoadMainWindow()
+        {
+            mainWindow = container.Resolve<MainWindow>();
+            SetupTabControl();
+            mainWindow.ShowDialog();
         }
 
         /// <summary>Tritt ein wenn ein neuer Tab geöffnet werden soll.</summary>
@@ -60,22 +101,8 @@
                 return;
             }
 
-            var tabItem = new TabItem
-                              {
-                                  Header = password.Account,
-                                  ToolTip = "Passwort bearbeiten",
-                                  Content = container.Resolve<PasswordView>()
-                              };
-
+            var tabItem = GetNewPasswordTab(password);
             mainWindow.TabControl.Items.Add(tabItem);
-        }
-
-        /// <summary>Lädt das Hauptfenster.</summary>
-        private void LoadMainWindow()
-        {
-            mainWindow = container.Resolve<MainWindow>();
-            SetupTabControl();
-            mainWindow.ShowDialog();
         }
 
         /// <summary>Registriert alle benötigten Typen beim IoC Container.</summary>
@@ -86,19 +113,32 @@
             container.RegisterType<IPanelViewModel, PanelViewModel>();
         }
 
-        /// <summary>Das Applikationsfenster.</summary>
-        private MainWindow mainWindow;
-
         /// <summary>Setzt das TabControl auf.</summary>
         private void SetupTabControl()
         {
-            mainWindow.TabControl.Items.Add(new TabItem { Header = "Passwörter", ToolTip = "Alle verwalteten Passwörter", Content = GetPanelView() });
+            var tabItem = GetNewTabItem("Passwörter", "Alle verwalteten Passwörter", GetPanelView());
+            mainWindow.TabControl.Items.Add(tabItem);
         }
 
         /// <summary>Startet die Anwendung.</summary>
         private void StartApplication()
         {
             LoadMainWindow();
+        }
+
+        /// <summary>Tritt ein wenn ein <strong>MouseUp Ereignis</strong> bei einem TabItem eintritt.</summary>
+        /// <param name="sender">Erwartet das TabItem.</param>
+        /// <param name="arguments">Prüft anhand der Eigenschaft ChangedButton welche Taste gedrückt wurde.</param>
+        private void TabItemClicked(object sender, MouseButtonEventArgs arguments)
+        {
+            var tabItem = sender as TabItem;
+
+            if (arguments.ChangedButton != MouseButton.Middle || tabItem == null || tabItem.Header.ToString() == "Passwörter")
+            {
+                return;
+            }
+
+            mainWindow.TabControl.Items.Remove(tabItem);
         }
 
         #endregion
