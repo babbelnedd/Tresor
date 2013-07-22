a5f5669de7cde61bcea4843193404735b7519996
Lucas Schad <schad.lucas@gmail.com>
2013-07-22 03:37:45 +0200

    Zwei IValueConverter implementiert.

---
+++ src/Tresor.Resources/Converter/BooleanToVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿namespace Tresor.Resources.Converter
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Data;
+
+    /// <summary>Konvertiert einen Boolean in eine Visibility. True wird zu Visible, False zu Collapsed.</summary>
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        /// <summary>Konvertiert einen Wert.</summary>
+        /// <returns>Ein konvertierter Wert. Wenn die Methode null zurückgibt, wird der gültige NULL-Wert verwendet.</returns>
+        /// <param name="value">Der von der Bindungsquelle erzeugte Wert.</param><param name="targetType">Der Typ der Bindungsziel-Eigenschaft.</param><param name="parameter">Der zu verwendende Konverterparameter.</param><param name="culture">Die im Konverter zu verwendende Kultur.</param>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        /// <summary>Konvertiert einen Wert.</summary>
+        /// <returns>Ein konvertierter Wert. Wenn die Methode null zurückgibt, wird der gültige NULL-Wert verwendet.</returns>
+        /// <param name="value">Der Wert, der vom Bindungsziel erzeugt wird.</param><param name="targetType">Der Typ, in den konvertiert werden soll.</param><param name="parameter">Der zu verwendende Konverterparameter.</param><param name="culture">Die im Konverter zu verwendende Kultur.</param>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
