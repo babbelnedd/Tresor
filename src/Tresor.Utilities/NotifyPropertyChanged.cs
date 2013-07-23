4a3c484b5489f024549cc9fc0334d9ebff6c5799
Lucas Schad <schad.lucas@gmail.com>
2013-07-23 18:04:48 +0200

    Kommentare und Regionen für NotifyPropertyChanged hinzugefügt.

--- src/Tresor.Utilities/NotifyPropertyChanged.cs
+++ src/Tresor.Utilities/NotifyPropertyChanged.cs
@@ -3,10 +3,20 @@
     using System.ComponentModel;
     using System.Runtime.CompilerServices;
 
+    /// <summary>Implementierung der INotifyPropertyChanged Schnittstelle.</summary>
     public class NotifyPropertyChanged : INotifyPropertyChanged
     {
+        #region Öffentliche Ereignisse
+
+        /// <summary>Ereignis, dass ausgelöst wird wenn sich eine Eigenschaft geändert hat.</summary>
         public event PropertyChangedEventHandler PropertyChanged;
 
+        #endregion
+
+        #region Methoden
+
+        /// <summary>Löst das PropertyChanged Ereignis aus.</summary>
+        /// <param name="propertyName">Der Name der Eigenschaft die sich geändert hat. Optional.</param>
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
         {
             if (PropertyChanged != null)
@@ -14,5 +24,7 @@
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
             }
         }
+
+        #endregion
     }
 }
