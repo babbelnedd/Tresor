464437c4102082e973a629f8882e9a461ded3b95
Lucas Schad <schad.lucas@gmail.com>
2013-07-26 13:14:56 +0200

    Überprüfung ob ein Passwort ungespeicherte Änderungen berücksichtigt nun den Fall, dass kein Klon existiert.

--- src/Tresor.Utilities/Password.cs
+++ src/Tresor.Utilities/Password.cs
@@ -1,177 +1,177 @@
 ﻿namespace Tresor.Utilities
 {
-    using System;
+   using System;
 
-    using Tresor.Contracts.Utilities;
+   using Tresor.Contracts.Utilities;
 
-    /// <summary>Stellt ein Passwort dar.</summary>
-    [Serializable]
-    public class Password : NotifyPropertyChanged, IPassword
-    {
-        #region Konstanten und Felder
+   /// <summary>Stellt ein Passwort dar.</summary>
+   [Serializable]
+   public class Password : NotifyPropertyChanged, IPassword
+   {
+      #region Konstanten und Felder
 
-        /// <summary>Mitglied der Eigenschaft <see cref="Account"/>.</summary>
-        private string account = string.Empty;
+      /// <summary>Mitglied der Eigenschaft <see cref="Account"/>.</summary>
+      private string account = string.Empty;
 
-        /// <summary>Der Klon des Passworts, der mit Beginn der Bearbeitung angelegt wird. Wird nach Abbruch oder Beendigung der Bearbeitung wieder geleert.</summary>
-        private Password clone;
+      /// <summary>Der Klon des Passworts, der mit Beginn der Bearbeitung angelegt wird. Wird nach Abbruch oder Beendigung der Bearbeitung wieder geleert.</summary>
+      private Password clone;
 
-        /// <summary>Mitglied der Eigenschaft <see cref="Description"/>.</summary>
-        private string description = string.Empty;
+      /// <summary>Mitglied der Eigenschaft <see cref="Description"/>.</summary>
+      private string description = string.Empty;
 
-        /// <summary>Mitglied der Eigenschaft <see cref="IsDirty"/>.</summary>
-        private bool isDirty;
+      /// <summary>Mitglied der Eigenschaft <see cref="IsDirty"/>.</summary>
+      private bool isDirty;
 
-        /// <summary>Mitglied der Eigenschaft <see cref="IsNew"/>.</summary>
-        private bool isNew;
+      /// <summary>Mitglied der Eigenschaft <see cref="IsNew"/>.</summary>
+      private bool isNew;
 
-        /// <summary>Mitglied der Eigenschaft <see cref="Key"/>.</summary>
-        private string key = string.Empty;
+      /// <summary>Mitglied der Eigenschaft <see cref="Key"/>.</summary>
+      private string key = string.Empty;
 
-        /// <summary>Mitglied der Eigenschaft <see cref="RecordID"/>.</summary>
-        private Guid recordID;
+      /// <summary>Mitglied der Eigenschaft <see cref="RecordID"/>.</summary>
+      private Guid recordID;
 
-        #endregion
+      #endregion
 
-        #region Öffentliche Eigenschaften
+      #region Öffentliche Eigenschaften
 
-        /// <summary>Holt oder setzt den <strong>Accountnamen</strong>.</summary>
-        public string Account
-        {
-            get
-            {
-                return account;
-            }
+      /// <summary>Holt oder setzt den <strong>Accountnamen</strong>.</summary>
+      public string Account
+      {
+         get
+         {
+            return account;
+         }
 
-            set
-            {
-                account = value;
-                OnPropertyChanged();
-            }
-        }
+         set
+         {
+            account = value;
+            OnPropertyChanged();
+         }
+      }
 
-        /// <summary>Holt oder setzt die <strong>Beschreibung</strong>.</summary>
-        public string Description
-        {
-            get
-            {
-                return description;
-            }
+      /// <summary>Holt oder setzt die <strong>Beschreibung</strong>.</summary>
+      public string Description
+      {
+         get
+         {
+            return description;
+         }
 
-            set
-            {
-                description = value;
-                OnPropertyChanged();
-            }
-        }
+         set
+         {
+            description = value;
+            OnPropertyChanged();
+         }
+      }
 
-        /// <summary>Holt oder setzt einen Wert, der angibt, ob das Passwort ungespeicherte Änderungen hat.</summary>
-        public bool IsDirty
-        {
-            get
-            {
-                return isDirty;
-            }
+      /// <summary>Holt oder setzt einen Wert, der angibt, ob das Passwort ungespeicherte Änderungen hat.</summary>
+      public bool IsDirty
+      {
+         get
+         {
+            return isDirty;
+         }
 
-            set
-            {
-                isDirty = value;
-                OnPropertyChanged();
-            }
-        }
+         set
+         {
+            isDirty = value;
+            OnPropertyChanged();
+         }
+      }
 
-        /// <summary>Holt oder setzt einen Wert, der angibt, ob das Passwort neu und ungespeichert ist.</summary>
-        public bool IsNew
-        {
-            get
-            {
-                return isNew;
-            }
+      /// <summary>Holt oder setzt einen Wert, der angibt, ob das Passwort neu und ungespeichert ist.</summary>
+      public bool IsNew
+      {
+         get
+         {
+            return isNew;
+         }
 
-            set
-            {
-                isNew = value;
-                OnPropertyChanged();
-            }
-        }
+         set
+         {
+            isNew = value;
+            OnPropertyChanged();
+         }
+      }
 
-        /// <summary>Holt oder setzt das eigentliche Passwort.</summary>
-        public string Key
-        {
-            get
-            {
-                return key;
-            }
+      /// <summary>Holt oder setzt das eigentliche Passwort.</summary>
+      public string Key
+      {
+         get
+         {
+            return key;
+         }
 
-            set
-            {
-                key = value;
-                OnPropertyChanged();
-            }
-        }
+         set
+         {
+            key = value;
+            OnPropertyChanged();
+         }
+      }
 
-        /// <summary>Die Eindeutige Kennung des Passworts.</summary>
-        public Guid RecordID
-        {
-            get
-            {
-                return recordID;
-            }
+      /// <summary>Die Eindeutige Kennung des Passworts.</summary>
+      public Guid RecordID
+      {
+         get
+         {
+            return recordID;
+         }
 
-            set
-            {
-                recordID = value;
-                OnPropertyChanged();
-            }
-        }
+         set
+         {
+            recordID = value;
+            OnPropertyChanged();
+         }
+      }
 
-        #endregion
+      #endregion
 
-        #region Öffentliche Methoden und Operatoren
+      #region Öffentliche Methoden und Operatoren
 
-        /// <summary>Beginnt die Bearbeitung eines Passworts.</summary>
-        public void BeginEdit()
-        {
-            clone = (Password)MemberwiseClone();
-        }
+      /// <summary>Beginnt die Bearbeitung eines Passworts.</summary>
+      public void BeginEdit()
+      {
+         clone = (Password)MemberwiseClone();
+      }
 
-        /// <summary>Bricht die Bearbeitung eines Passworts ab. Das Passwort wird auf seinen Ursprungszustand zurückgesetzt.</summary>
-        public void CancelEdit()
-        {
-            // Note: Moment mal.. Will ich denn alles zurücksetzen? Eigentlich nicht! Hier Parameter fragen welche eigenschaft zurückgesetzt werden soll?
-            Description = clone.Description;
-            Key = clone.Key;
-            Account = clone.Account;
-            IsDirty = false; // Note: siehe erste Note! Könnte ja schon was Dirty sein?!
-            clone = null;
-        }
+      /// <summary>Bricht die Bearbeitung eines Passworts ab. Das Passwort wird auf seinen Ursprungszustand zurückgesetzt.</summary>
+      public void CancelEdit()
+      {
+         // Note: Moment mal.. Will ich denn alles zurücksetzen? Eigentlich nicht! Hier Parameter fragen welche eigenschaft zurückgesetzt werden soll?
+         Description = clone.Description;
+         Key = clone.Key;
+         Account = clone.Account;
+         IsDirty = false; // Note: siehe erste Note! Könnte ja schon was Dirty sein?!
+         clone = null;
+      }
 
-        /// <summary>Beendet die Bearbeitung eines Passworts.</summary>
-        public void EndEdit()
-        {
-            clone = null;
-            IsDirty = true;
-        }
+      /// <summary>Beendet die Bearbeitung eines Passworts.</summary>
+      public void EndEdit()
+      {
+         clone = null;
+         IsDirty = true;
+      }
 
-        /// <summary>Holt einen Wert, der angibt, ob das Passwort schmutzig ist.</summary>
-        /// <returns>True wenn ungespeicherte Änderungen ausstehen, andernfalls False.</returns>
-        public bool IsCloneEqual()
-        {
-            if (Account != clone.Account || Key != clone.Key || Description != clone.Description)
-            {
-                return false;
-            }
+      /// <summary>Holt einen Wert, der angibt, ob das Passwort schmutzig ist.</summary>
+      /// <returns>True wenn ungespeicherte Änderungen ausstehen, andernfalls False.</returns>
+      public bool IsCloneEqual()
+      {
+         if (clone == null || Account != clone.Account || Key != clone.Key || Description != clone.Description)
+         {
+            return false;
+         }
 
-            return true;
-        }
+         return true;
+      }
 
-        /// <summary>Überschreibt das Basisverhalten.</summary>
-        /// <returns>Gibt den <see cref="Account"/> zurück.</returns>
-        public override string ToString()
-        {
-            return Account;
-        }
+      /// <summary>Überschreibt das Basisverhalten.</summary>
+      /// <returns>Gibt den <see cref="Account"/> zurück.</returns>
+      public override string ToString()
+      {
+         return Account;
+      }
 
-        #endregion
-    }
+      #endregion
+   }
 }
