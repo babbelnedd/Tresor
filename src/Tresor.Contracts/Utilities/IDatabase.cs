1a5e31f19c1aa03ca2c2c273d4c6c3db0d2b1014
Lucas Schad <schad.lucas@gmail.com>
2013-08-01 23:53:06 +0200

    Schnittstelle für Datenbank und Benutzereinstellungen angelegt.
     #TRS-11

---
+++ src/Tresor.Contracts/Utilities/IDatabase.cs
@@ -0,0 +1,52 @@
+﻿namespace Tresor.Contracts.Utilities
+{
+    using System.Collections.Generic;
+
+    /// <summary>Schnittstelle für eine Datenbank.</summary>
+    public interface IDatabase
+    {
+        #region Public Properties
+
+        /// <summary>Setzt den Namen der Datenbank.</summary>
+        /// <param name="name">Der zu setzende Name.</param>
+        void SetName(string name);
+
+        /// <summary>Setzt den Pfad der Datenbank. Optional.</summary>
+        /// <param name="path">Der Pfad wo die Datenbank zu finden bzw. anzulegen ist.</param>
+        void SetPath(string path);
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        /// <summary>Schließt die Verbindung zur Datenbank.</summary>
+        void CloseConnection();
+
+        /// <summary>Zerstört die Verbindung zur Datenbank.</summary>
+        void DisposeConnection();
+
+        /// <summary>Setzt einen SQL Befehl auf die Datenbank ab.</summary>
+        /// <param name="command">Der auszuführende Befehl.</param>
+        void Execute(string command);
+
+        /// <summary>Setzt einen SQL Befehl auf die Datenbank ab und liefert eine Ergebnismenge zurück.</summary>
+        /// <param name="command">Der auszuführende Befehl.</param>
+        /// <returns>Eine Auflistung aller Ergebnisse.</returns>
+        /// <remarks>Die äußere Liste enthält alle Ergebnisse. Die innere Liste enhält die Werte aller abgefragten Felder.</remarks>
+        /// <example>
+        /// Die Tabelle Person hat 5 Einträge
+        /// SELECT Name, LastName, Age, Sex FROM Person
+        /// Ergebnis:   Äußere Liste hat 5 Einträge. (Enthält alle Ergebnisse)
+        ///             Die einzelnen Listen haben jeweils 4 Einträge. (Für jedes abgefragte Feld der Tabelle Person)
+        /// </example>
+        List<List<string>> Get(string command);
+
+        /// <summary>Öffnet die Verbindung zur Datenbank.</summary>
+        void OpenConnection();
+
+        /// <summary>Setzt das Passwort der Datenbank.</summary>
+        void SetPassword();
+
+        #endregion
+    }
+}
---
+++ src/Tresor.Contracts/Utilities/IUserSettings.cs
@@ -0,0 +1,50 @@
+﻿namespace Tresor.Contracts.Utilities
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>Schnittstelle für die Verwaltung von Benutzereinstellungen.</summary>
+    public interface IUserSettings
+    {
+        #region Public Methods and Operators
+
+        /// <summary>Legt eine neue Kategorie an.</summary>
+        /// <param name="category">Der Name der Kategorie.</param>
+        /// <exception cref="Exception">Die Kategorie existiert bereits.</exception>
+        void Add(string category);
+
+        /// <summary>Fügt einer Kategorie einen Schlüssel hinzu.</summary>
+        /// <param name="category">Die Kategorie welcher ein Schlüssel hinzugefügt werden soll.</param>
+        /// <param name="key">Der Name des Schlüssels.</param>
+        /// <param name="value">Der einzutragende Wert.</param>
+        void Add(string category, string key, string value);
+
+        /// <summary>Holt alle Key-Value Paare.</summary>
+        /// <param name="catgeory">Die Kategorie welche ausgelesen werden soll.</param>
+        /// <returns>Alle Key-Value Paare aus einer bestimmten Kategorie.</returns>
+        Dictionary<string, string> Read(string catgeory);
+
+        /// <summary>Holt den Wert zu einem bestimmten Schlüssel.</summary>
+        /// <param name="category">Die Kategorie, in der sich der Schlüssel befindet.</param>
+        /// <param name="key">Der auszulesende Schlüssel.</param>
+        /// <returns>Der Wert zu dem Schlüssel.</returns>
+        string Read(string category, string key);
+
+        /// <summary>Überschreibt einen Wert.</summary>
+        /// <param name="category">Die Kategorie, in der sich der Schlüssel befindet.</param>
+        /// <param name="key">Der Schlüssel, dessen Wert überschrieben werden soll.</param>
+        /// <param name="value">Der neue Wert.</param>
+        /// <remarks>Es wird kein neuer Schlüssel angelegt.</remarks>
+        void Write(string category, string key, string value);
+
+        /// <summary>Überschreibt einen Wert.</summary>
+        /// <param name="category">Die Kategorie in welcher das Key-Value Paar gespeichert werden soll.</param>
+        /// <param name="pair">Kategorie und Schlüssel welcher überschrieben werden soll.</param>
+        /// <remarks>Es wird kein neuer Schlüssel angelegt.</remarks>
+        /// <exception cref="Exception">Die Kategorie existiert nicht.</exception>
+        /// <exception cref="Exception">Der Wert existiert bereits.</exception>
+        void Write(string category, Dictionary<string, string> pair);
+
+        #endregion
+    }
+}
