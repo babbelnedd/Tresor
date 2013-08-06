14d8588866312448b9139f80032e78c75e5be6fa
Lucas Schad <schad.lucas@gmail.com>
2013-08-06 18:03:54 +0200

    Bootloader auf XML Konfiguration umgestellt.

--- src/Tresor/Bootstrapper.cs
+++ src/Tresor/Bootstrapper.cs
@@ -1,18 +1,10 @@
 ﻿namespace Tresor
 {
-    using System.Windows.Controls;
-
     using Microsoft.Practices.Unity;
+    using Microsoft.Practices.Unity.Configuration;
 
     using Tresor.Contracts.Model;
-    using Tresor.Contracts.ViewModel;
-    using Tresor.Model;
-    using Tresor.Utilities.Mediator;
-    using Tresor.View;
-    using Tresor.View.Application;
-    using Tresor.View.UserControls;
-    using Tresor.ViewModel;
-    using Tresor.ViewModel.Application;
+    using Tresor.Contracts.Model.Application;
 
     /// <summary>Statischer Bootstrapper der die Anwendung verkabelt.</summary>
     internal class Bootstrapper
@@ -22,9 +14,6 @@
         /// <summary>Der IoC Container.</summary>
         private readonly IUnityContainer container = new UnityContainer();
 
-        /// <summary>Das Applikationsfenster.</summary>
-        private MainWindow mainWindow;
-
         #endregion
 
         #region Methods
@@ -32,67 +21,10 @@
         /// <summary>Lädt die Anwendung.</summary>
         internal void LoadApplication()
         {
-            RegisterAll();
-            LoadSplashView();
-        }
-
-        /// <summary>Initialisiert die SplashView.</summary>
-        /// <returns>Die instanzierte SplashView.</returns>
-        private SplashView InitializeSplashView()
-        {
-            var view = container.Resolve<SplashView>();
-            var viewModel = container.Resolve<SplashViewModel>();
-            view.DataContext = viewModel;
-            viewModel.PropertyChanged += (sender, arguments) =>
-                {
-                    if (arguments.PropertyName == "KeyIsCorrect" && viewModel.KeyIsCorrect)
-                    {
-                        view.Close();
-                        LoadMainWindow();
-                    }
-                };
-            return view;
-        }
-
-        /// <summary>Lädt das Hauptfenster.</summary>
-        private void LoadMainWindow()
-        {
-            mainWindow = container.Resolve<MainWindow>();
-            var model = container.Resolve<IPanelModel>();
-            container.RegisterInstance(new MainViewModel(model));
-            var mainViewModel = container.Resolve<MainViewModel>();
-
-            container.RegisterInstance(new PasswordMediator());
-            var mediator = container.Resolve<PasswordMediator>();
-            container.RegisterInstance<UserControl>("PasswordView", new PasswordView());
-            container.RegisterInstance<UserControl>("PasswordListView", new PasswordListView());
-            container.RegisterInstance<IPasswordViewModel>("PasswordViewModel", new PasswordViewModel(model, mediator));
-            container.RegisterInstance<IPasswordListViewModel>("PasswordListViewModel", new PasswordListViewModel(mediator));
-
-            var passwordViewModel = container.Resolve<IPasswordViewModel>("PasswordViewModel");
-            var passwordListViewModel = container.Resolve<IPasswordListViewModel>("PasswordListViewModel");
-
-            mediator.Add(passwordViewModel);
-            mediator.Add(passwordListViewModel);
-            mediator.Add(mainViewModel);
-
-            mainViewModel.Container = container;
-            mainWindow.DataContext = mainViewModel;
-            mainWindow.ShowDialog();
-        }
-
-        /// <summary>Lädt die SplashView.</summary>
-        /// <remarks>Bei erfolgreicher Eingabe des Schlüssels zum Laden der Passwörter wird das MainWindow geladen.</remarks>
-        private void LoadSplashView()
-        {
-            var view = InitializeSplashView();
-            view.ShowDialog();
-        }
-
-        /// <summary>Registriert alle benötigten Typen beim IoC Container.</summary>
-        private void RegisterAll()
-        {
-            container.RegisterInstance<IPanelModel>(new SqlitePanelModel("tresor.db"));
+            container.LoadConfiguration();
+            var y = container.Resolve<ISplashModel>("SplashModel");
+            var x = container.Resolve<IMainModel>("MainModel");
+            var z = container.Resolve<IPasswordModel>("PasswordModel");
         }
 
         #endregion
