a6e0deada1dea35ba9d923063744f9bf5756d6e5
Lucas Schad <schad.lucas@gmail.com>
2013-09-23 01:49:08 +0200

    ClickOnceUpdateManager implementiert.

---
+++ src/Tresor.Utilities/UpdateManager.cs
@@ -0,0 +1,69 @@
+﻿namespace Tresor.Utilities
+{
+    using System.Deployment.Application;
+    using System.Timers;
+
+    public class ClickOnceUpdateManager
+    {
+        private ApplicationDeployment currentDeployment;
+        private Timer timer = new Timer(60000);
+
+        public bool IsUpdating { get; private set; }
+
+        public double UpdateInterval
+        {
+            get { return timer.Interval; }
+            set { timer.Interval = value; }
+        }
+
+
+
+        public ClickOnceUpdateManager()
+        {
+            timer.Elapsed += (obj, sender) => CheckForUpdate();
+            currentDeployment.UpdateCompleted += (obj, sender) => UpdateCompleted();
+            currentDeployment.CheckForUpdateCompleted += UpdateCheckCompleted;
+        }
+
+
+
+        public void Start()
+        {
+            timer.Start();
+        }
+
+        public void Stop()
+        {
+            timer.Stop();
+        }
+
+
+
+        private void CheckForUpdate()
+        {
+            if (IsUpdating) return;
+            currentDeployment.CheckForUpdateAsync();
+        }
+
+        private void UpdateCompleted()
+        {
+            IsUpdating = false;
+            // TODO: Über Update nach Außen informieren
+        }
+
+        private void UpdateCheckCompleted(object sender, CheckForUpdateCompletedEventArgs arguments)
+        {
+            if (arguments.UpdateAvailable)
+            {
+                Update();
+            }
+        }
+
+        private void Update()
+        {
+            IsUpdating = true;
+            currentDeployment.UpdateAsync();
+            // TODO: Über Update Vorgang nach Außen informieren
+        }
+    }
+}
--- src/Tresor/App.cs
+++ src/Tresor/App.cs
@@ -1,63 +1,24 @@
 ﻿namespace Tresor
 {
     using System;
-    using System.ComponentModel;
-    using System.Deployment.Application;
-    using System.Timers;
     using System.Windows;
 
     using Logging;
+
+    using Utilities;
 
     /// <summary>Grundgerüst der Anwendung.</summary>
     internal class App : Application
     {
         #region Public Methods and Operators
 
-        private static bool isUpdating;
-
         /// <summary>Haupteinstiegspunkt der Anwendung.</summary>
         [STAThread]
         public static void Main()
         {
-            CheckForUpdates();
+            var updateManager = new ClickOnceUpdateManager();
+            updateManager.Start();
             RunApplication();
-        }
-
-        /// <summary>Prüft ob es Updates von der Anwendung gibt.</summary>
-        private static void CheckForUpdates()
-        {
-            var ad = ApplicationDeployment.CurrentDeployment;
-            ad.CheckForUpdateCompleted += CheckForUpdateCompleted;
-            ad.UpdateCompleted += UpdateCompleted;
-            ad.CheckForUpdateAsync();
-
-            var timer = new Timer();
-            timer.Interval = 10000;
-            timer.Start();
-            timer.Elapsed += TimerElapsed;
-        }
-
-        private static void TimerElapsed(object sender, ElapsedEventArgs arguments)
-        {
-            if (!isUpdating)
-            {
-                ApplicationDeployment.CurrentDeployment.CheckForUpdateAsync();
-            }
-        }
-
-        private static void UpdateCompleted(object sender, AsyncCompletedEventArgs arguments)
-        {
-            isUpdating = false;
-            MessageBox.Show("Update fertig.");
-        }
-
-        private static void CheckForUpdateCompleted(object sender, CheckForUpdateCompletedEventArgs arguments)
-        {
-            if (arguments.UpdateAvailable)
-            {
-                isUpdating = true;
-                ApplicationDeployment.CurrentDeployment.UpdateAsync();
-            }
         }
 
         #endregion
